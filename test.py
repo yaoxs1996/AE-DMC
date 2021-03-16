1c7cd63b45b8747f934e32c430044dd588cbc10c
Infinity_Yao <907303127@qq.com>
2021-03-16 10:24:15 +0800

    测试

--- test.py
+++ test.py
@@ -15,55 +15,42 @@ from AutoEncoder import Autoencoder
 
 random.seed(42)
 
-def split_novel(data, labels, n_known, n_novel):
-    classes = np.unique(labels)
-    train_set = sample(classes.tolist(), n_known)
-    remain_set = list(set(classes) - set(train_set))
-    test_set = sample(remain_set, n_novel)
-
-    train_index = [i in train_set for i in labels]
-    test_index = [i in test_set for i in labels]
-    train_ds = data[train_index, :]
-    #train_labels = labels[train_index]
-    novel_ds = data[test_index, :]
-    #novel_labels = labels[test_index]
-
-    train_ds, extra_ds = train_test_split(train_ds, test_size=0.4, random_state=42)
-    test_ds = np.r_[extra_ds, novel_ds]
-    y_train = np.ones(train_ds.shape[0])
-    #y_test = np.ones(test_ds.shape[0]) * -1
-    y_test = np.r_[np.ones(extra_ds.shape[0]), np.ones(novel_ds.shape[0])*(-1)]
-    #test_labels = np.r_[extra_labels, novel_labels]
-
-    return train_ds, y_train, test_ds, y_test
-
 def letter_exp():
-    letter = pd.read_csv("./dataset/letter_csv.csv")
-    data = letter.to_numpy()
-    labels = data[:, -1]
-    data = data[:, :-1]
-    le = LabelEncoder()
-    labels = le.fit_transform(labels)
-    #train_ds, train_labels, test_ds, test_labels = split_novel(data, labels, 10, 8)
-    x_train, y_train, x_test, y_test = split_novel(data, labels, 10, 8)
+    x, y = utils.load_data("letter_csv")
+    x_train, y_train, x_test, y_test = utils.split_train_test(x, y, 18)
+    x_train, y_train, x_val, y_val = utils.split_train_val(x_train, y_train)
 
     scaler = MinMaxScaler()
     x_train = scaler.fit_transform(x_train)
     x_test = scaler.transform(x_test)
 
-    autoencoder = Autoencoder(input_dim=x_train.shape[1], h1=12, h2=8, latent_dim=4)
-    autoencoder.compile(optimizer="adam", loss=losses.MeanSquaredError())
-    autoencoder.fit(x_train, x_train, epochs=5, shuffle=True, validation_data=(x_test, x_test))
+    # autoencoder = Autoencoder(input_dim=x_train.shape[1], h1=12, h2=8, latent_dim=4)
+    # autoencoder.compile(optimizer="adam", loss=losses.MeanSquaredError())
+    # autoencoder.fit(x_train, x_train, epochs=5, shuffle=True, validation_data=(x_test, x_test))
 
-    x_train = autoencoder.encoder(x_train)
-    x_test = autoencoder.encoder(x_test)
+    # x_train = autoencoder.encoder(x_train)
+    # x_test = autoencoder.encoder(x_test)
+
+    autoencoder, encoder = AutoEncoder.build_model(x_train, y_train)
+    autoencoder.compile(optimizer="adam",
+        loss={
+            "decoder": losses.MeanSquaredError(),
+            "classifier": losses.SparseCategoricalCrossentropy(from_logits=True),
+        },
+        metrics={
+            "classifier": "accuracy"
+        })
+    autoencoder.fit(x_train, {"classifier": y_train, "decoder": x_train}, epochs=20, batch_size=32)
+
+    x_train = encoder(x_train)
+    x_test = encoder(x_test)
 
     model = DMCluster(nb_mirco_cluster=100, radius_factor=1.5, new_radius=0.9)
     print("开始训练")
     model.fit(x_train, y_train)
     print("开始检测")
-    y_pred = model.predict(x_test)
-    print(f1_score(y_test, y_pred, average="micro"))
+    y_pred = model.predict(x_val)
+    print(f1_score(y_val, y_pred, average="micro"))
     print(confusion_matrix(y_test, y_pred))
 
 def test():
@@ -85,5 +72,5 @@ def test():
     model.fit(x, {"classifier": y, "decoder": x}, epochs=20, batch_size=32)
 
 if __name__ == "__main__":
-    # letter_exp()
-    test()
+    letter_exp()
+    # test()
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+from random import sample
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 def load_data(name):
     data = pd.read_csv("./dataset/" + name + ".csv")
@@ -15,3 +17,47 @@ def load_data(name):
     y = y.astype(np.int)
 
     return x, y
+
+def split_train_test(x, y, omega):
+    n_classes = np.unique(y).tolist()
+    train_set = sample(n_classes, omega)
+    test_set = list(set(n_classes) - set(train_set))
+
+    train_index = [i in train_set for i in y]
+    test_index = [i in test_set for i in y]
+
+    x_train = x[train_index, :]
+    y_train = y[train_index]
+
+    x_test = x[test_index, :]
+    y_test = y[test_index]
+
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.4, shuffle=True, stratify=y_train)
+
+    x_test = np.r_[x_test, x_val]
+    y_test = np.r_[np.ones(x_test.shape[0])*(-1), np.ones(x_val.shape[0])]
+
+    return x_train, y_train, x_test, y_test
+
+def split_train_val(x, y):
+    n_classes = np.unique(y).to_list()
+    n_known = int(n_classes / 2 + 0.5)
+
+    train_set = sample(n_classes, n_known)
+    val_set = list(set(n_classes) - set(train_set))
+
+    train_index = [i in train_set for i in y]
+    val_index = [i in val_set for i in y]
+
+    x_train = x[train_index, :]
+    y_train = y[train_index]
+
+    x_val = x[val_index, :]
+    y_val = y[val_index]
+
+    x_train, x_extra, y_train, _ = train_test_split(x_train, y_train, test_size=0.4, shuffle=True, stratify=y_train)
+
+    x_val = np.r_[x_extra, x_val, x_extra]
+    y_val = np.r_[np.ones(x_extra.shape[0]), np.ones(x_val.shape[0])*(-1), np.ones(x_extra.shape[0])]
+
+    return x_train, y_train, x_val, y_val
